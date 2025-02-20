b94fed3a9b2fa5b00d62fd7c661515629e07c823
Eddie Garcia <eddieagvictoria@gmail.com>
2025-02-20 14:22:52 -0600

    add Array Response
    changes about the comment and success

--- www/AndroidButtonHome.js
+++ www/AndroidButtonHome.js
@@ -2,7 +2,22 @@ var exec = require('cordova/exec');
 
 var AndroidButtonHome = {
     simulateHomeButton: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AndroidButtonHome", "simulateHomeButton", []);
+        exec(
+            function(result) {
+                // result es un objeto que contiene { success: boolean, message: string }
+                if (result.success) {
+                    successCallback(result.message); // Éxito
+                } else {
+                    errorCallback(result.message); // Error
+                }
+            },
+            function(error) {
+                errorCallback(error); // Error en la ejecución
+            },
+            "AndroidButtonHome",
+            "simulateHomeButton",
+            []
+        );
     }
 };
 
