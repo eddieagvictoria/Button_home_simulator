32adfcf50f12dd9713d54365c1c5f1351dede496
Eddie Garcia <eddieagvictoria@gmail.com>
2025-02-20 17:46:25 -0600

    update java file
    update the java file and the js file

--- src/android/AndroidButtonHome.java
+++ src/android/AndroidButtonHome.java
@@ -5,7 +5,6 @@ import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.content.Intent;
 import android.app.Activity;
@@ -22,27 +21,10 @@ public class AndroidButtonHome extends CordovaPlugin {
     }
 
     private void simulateHomeButton(CallbackContext callbackContext) {
-        try {
-            Activity activity = this.cordova.getActivity();
-            Intent intent = new Intent(Intent.ACTION_MAIN);
-            intent.addCategory(Intent.CATEGORY_HOME);
-            activity.startActivity(intent);
-
-            // Crear un objeto JSON con el resultado
-            JSONObject result = new JSONObject();
-            result.put("success", true); // Éxito
-            result.put("message", "Home button simulated successfully");
-
-            // Enviar el resultado a JavaScript
-            callbackContext.success(result);
-        } catch (Exception e) {
-            // En caso de error, enviar un mensaje de error
-            JSONObject result = new JSONObject();
-            result.put("success", false); // Error
-            result.put("message", "Failed to simulate home button: " + e.getMessage());
-
-            // Enviar el resultado a JavaScript
-            callbackContext.error(result);
-        }
+        Activity activity = this.cordova.getActivity();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        activity.startActivity(intent);
+        callbackContext.success("Home button simulated");
     }
 }
--- www/AndroidButtonHome.js
+++ www/AndroidButtonHome.js
@@ -2,22 +2,7 @@ var exec = require('cordova/exec');
 
 var AndroidButtonHome = {
     simulateHomeButton: function(successCallback, errorCallback) {
-        exec(
-            function(result) {
-                // result es un objeto que contiene { success: boolean, message: string }
-                if (result.success) {
-                    successCallback(result.message); // Éxito
-                } else {
-                    errorCallback(result.message); // Error
-                }
-            },
-            function(error) {
-                errorCallback(error); // Error en la ejecución
-            },
-            "AndroidButtonHome",
-            "simulateHomeButton",
-            []
-        );
+        exec(successCallback, errorCallback, "AndroidButtonHome", "simulateHomeButton", []);
     }
 };
 
